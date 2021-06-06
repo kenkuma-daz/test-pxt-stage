9b7b7696c520b883c1d26d039c3e12873f3d2bbd
kenkuma-daz <80566958+kenkuma-daz@users.noreply.github.com>
2021-06-06 22:34:42 +0900

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,25 +3,7 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "image1":
-            case "forestMonkeyTile":return img`
-f f f f f f f f f f f f f f f f 
-f f f f e e e e e f f f f f f f 
-f f f d d d d e e e f f f f f f 
-f c d f d d f d e e f f f f f f 
-f c d f d d f d e e d d f f f f 
-c d e e d d d d e e b d c f f f 
-c d d d d c d d e e b d c f f f 
-c c c c c d d d e e f c f f e f 
-f f d d d d d e e f f f f f e f 
-f f f f f f f e e e e f f f e f 
-f f f f f e e e e e e e f f e f 
-f f f f e f f e f e e e e f f f 
-f f f f e f f e f e e e e f f f 
-f f f f d b f d b f f e f f f f 
-f f f f d d c d d b b d f f f f 
-f f f f f f f f f f f f f f f f 
-`;
+
         }
         return null;
     })
